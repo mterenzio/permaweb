48b22ce43e95516fce91c9ddfe5c78f23f58617e
candye_sweet <olya.melska@gmail.com>
2019-07-11 21:14:44 -0400

    Changed the request to work for any links and return content as markdown

--- controllers/Link/index.js
+++ controllers/Link/index.js
@@ -1,18 +1,15 @@
 const Mercury = require("@postlight/mercury-parser");
 
 module.exports = {
-  getArticle: (req, res, next) => {
-    const title = req.body.title.split(" ").join("-");
-    const url = `https://medium.com/the-node-js-collection/${title}`;
-    console.log("URL is ", url);
-    Mercury.parse(url)
+  getLink: (req, res, next) => {
+    Mercury.parse(req.body.url, { contentType: "markdown" })
       .then(result => {
         console.log(`Inside Mercury!\n${result}`);
         res.json(result);
       })
       .catch(err => {
         res.status(404).json({
-          error: "Mistake happened!"
+          error: "Invalid url"
         });
       });
   }
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const ArticleController = require("../controllers/Article");
+const LinkController = require("../controllers/Link");
 
 /* GET home page. */
 router.route("/").get((req, res, next) => {
   res.json("Home page");
 });
 
-router.route("/article").post(ArticleController.getArticle);
+router.route("/link").post(LinkController.getLink);
 
 module.exports = router;
