5ec9707c11bca56066cd7b827305065e105652f6
Matt Terenzio <matt@thetylt.com>
2019-07-20 09:23:25 -0400

    adds getFeed and renames link to source

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const LinkController = require("../controllers/Link");
+const SourceController = require("../controllers/Source");
 
 /* GET home page. */
 router.route("/").get((req, res, next) => {
   res.json("Home page");
 });
 
-router.route("/link").post(LinkController.getLink);
+router.route("/article").post(SourceController.getArticle);
+router.route("/feed").post(SourceController.getFeed);
 
 module.exports = router;
