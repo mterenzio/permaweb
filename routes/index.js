6cbc557016172d264730f4d7081c55dee7a8e91c
Matt Terenzio <matt@thetylt.com>
2019-09-30 21:16:11 -0400

    removes mercury

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const SourceController = require("../controllers/Source");
+//const SourceController = require("../controllers/Source");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'api.open.news' });
 });
 
-
-router.route("/article").post(SourceController.getArticle);
+//router.route("/article").post(SourceController.getArticle);
 
 module.exports = router;
