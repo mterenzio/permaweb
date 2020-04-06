1c7a5a7cbebebb357ff7d596f7231c421ddbbae0
Matt Terenzio <mterenzio@gmail.com>
2020-04-05 22:50:26 -0400

    uses cheerio for linked data

--- controllers/LinkedData/index.js
+++ controllers/LinkedData/index.js
@@ -1,14 +1,25 @@
 const request = require('request');
-const { JSDOM } = require("jsdom");
+const cheerio = require('cheerio');
 
-module.exports = {
-
-  getLinkedData: (req, res, next) => {
-    request(req.body.url, function(error, response, body) {
-      const dom = new JSDOM(body);
-      var jsonld = JSON.parse(dom.window.document.querySelector('script[type="application/ld+json"]').textContent)
-      res.json(jsonld);
-    });
-  }
-
-};
+function getLinkedData (req, res, next) {
+  let new_url = req.body.url;
+  request(new_url, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var $ = cheerio.load(body);
+      var obj = $("script[type='application/ld+json']");
+      for (var i in obj){
+        for (var j in obj[i].children) {
+          var data = JSON.parse(obj[i].children[j].data);
+            if (data) {
+              console.log(data);
+              res.json(data);
+            } else {
+              res.status(404).json({
+                error: "error fetching article data"
+              });
+            }
+         }
+      }
+    }
+  });
+}
